f4647a3f0e72438c2d1fd182934c7fc324c5c7f2
Li Tian <1518061368@qq.com>
2025-03-20 16:01:08 +0800

    Initial commit

---
+++ Assets/Li Tian Folder/Li Tian Script/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float moveX;
+    private float moveY;
+    private float moveSpeed = 15;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public void OnMove(InputValue moveValue)
+    {
+        Vector2 moveVector = moveValue.Get<Vector2>();
+        moveX = moveVector.x;
+        moveY = moveVector.y;
+    }
+    public void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveX, 0.0f, moveY);
+        rb.AddForce(movement * moveSpeed);
+    }
+
+}
